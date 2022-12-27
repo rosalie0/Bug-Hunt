5ed16b3818580fb682237f92b779d9c2dcae9b89
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-26 19:32:38 -0500

    butterfly animates on loop. the textures clip weirdly though

--- src/components/CanvasContainer.jsx
+++ src/components/CanvasContainer.jsx
@@ -4,22 +4,25 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-import Stickbug from "./Stickbug";
+// import Stickbug from "./Stickbug";
 import Loading from "./Loading";
+import Butterfly from "./Butterfly";
 
 function CanvasContainer() {
-  const bugPosition = [0, 5, 0]; // x, y, z
+  //const bugPosition = [0, 5, 0]; // x, y, z
+
   return (
     <div className="canvasContainer">
-      <Canvas camera={{ position: [10, 5, 10] }}>
-        <Suspense fallback={<Loading />}>
+      <Suspense fallback={<Loading />}>
+        <Canvas camera={{ position: [10, 5, 10] }}>
           <OrbitControls />
           {/* Models */}
-          <Stickbug sizeOfBug={10} bugPosition={bugPosition} />
+          {/* <Stickbug sizeOfBug={10} bugPosition={bugPosition} /> */}
+          <Butterfly scale={10} />
           {/* Helpers */}
           <gridHelper args={[40, 40, 0xff0000, "teal"]} />
-        </Suspense>
-      </Canvas>
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
